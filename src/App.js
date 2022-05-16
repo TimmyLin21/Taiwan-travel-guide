0509e4b9ce7aa96250a11da06c0a651780533455
TimmyLin21 <f7964032@gmail.com>
2022-05-16 11:39:02 +0800

    feat:新增頁面 lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { ResetStyle, GlobalStyles } from "./components/styles/Global";
 import Layout from "./components/layout/Layout";
-import Front from "./pages/Front";
-import SearchPage from "./pages/SearchPage";
-import DetailPage from "./pages/DetailPage";
+// import Front from "./pages/Front";
+// import SearchPage from "./pages/SearchPage";
+// import DetailPage from "./pages/DetailPage";
+// import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
-import NotFound from "./pages/NotFound";
 import Loading from "./components/loading/Loading";
 import { useSelector } from 'react-redux';
 import 'animate.css';
+import React, { Suspense } from 'react';
+
+const Front = React.lazy(() => import('./pages/Front'));
+const SearchPage = React.lazy(() => import("./pages/SearchPage"));
+const DetailPage = React.lazy(() => import("./pages/DetailPage"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   const status = useSelector((state) => state.loading.status);
@@ -18,12 +24,14 @@ function App() {
       <GlobalStyles />
       {status === 'pending'?<Loading />: ''}
       <Layout>
-        <Routes>
-          <Route path="/" element={<Front />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/detail" element={<DetailPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Front />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/detail" element={<DetailPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </>
   );
